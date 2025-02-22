6975d594860d888d449938b377de569c1329c8b5
Justin Scott <justin.scott@revelry.co>
2025-02-21 23:08:55 -0600

    Fixing empty spaces.

--- src/triggers/ErionUI/9-capture/CopytoBuffer.lua
+++ src/triggers/ErionUI/9-capture/CopytoBuffer.lua
@@ -1,4 +1,12 @@
+local currentLineEmpty = getCurrentLine():gsub("%s*", "")
+moveCursorEnd("MainBuffer")
+moveCursorUp("MainBuffer", 1, false)
+local mainBufferLineEmpty = getCurrentLine("MainBuffer"):gsub("%s*","")
+
+if currentLineEmpty == "" and currentLineEmpty == mainBufferLineEmpty then
+  deleteLine()
+else
   selectCurrentLine()
   copy()
   appendBuffer("MainBuffer")
-  
+end
