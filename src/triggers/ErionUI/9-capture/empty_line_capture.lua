2149aee5c52af5d92c443025ba058551ce80e541
Justin Scott <justin.scott@revelry.co>
2025-02-21 23:08:55 -0600

    Fixing empty spaces.

--- src/triggers/ErionUI/9-capture/empty_line_capture.lua
+++
@@ -1,7 +0,0 @@
-local emptyLine = getCurrentLine():gsub("%s*", "")
-moveCursorEnd("MainBuffer")
-local maybeEmptyLine = getCurrentLine("MainBuffer"):gsub("%s*","")
-
-if empyLine == maybeEmptyLine then
-  deleteLine()
-end
